20f194bfef881dcd76050eb5afb97523921c1dbf
KimHyunjun <khj@zeonplex.co.kr>
2019-11-03 01:20:35 +0900

    강의 1편 코드

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Main from "./container/Main";
+import WebtoonHome from "./container/WebtoonHome";
+import Viewer from "./container/Viewer";
+
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Route exact path="/" component={Main} />
+                    <Route path="/webtoon/:webtoonId" component={WebtoonHome} />
+                    <Route path="/viewer/:episodeId" component={Viewer} />
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
